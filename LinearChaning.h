56eb6af69610c170ed78dc61e431015e47841d34
swiftomkar <swiftomkar@gmail.com>
2018-04-27 13:33:21 -0400

    grow() in progress

--- LinearChaning.h
+++ LinearChaning.h
@@ -13,20 +13,25 @@ private:
     linkedlist* arr;
     int size;
     int capacity;
-    /*void grow(){
+    void grow(){
         linkedlist* bkp=arr;
+        int bkpCapacity=capacity;
         arr=new linkedlist[capacity*2];
         capacity=2*capacity;
         for(int i=0;i<capacity;i++)
             arr[i].head= nullptr;
-        for(int i=0;i<size;i++)
+        for(int i=0;i<bkpCapacity;i++)
             if(bkp[i].head!= nullptr) {
+                linkedlist::iterator iter(bkp[i]);
+                while(!iter){
+                    add(*iter);
+                }
                 //uint64_t pos = SpookyHash::Hash64(&(bkp[i].head), capacity,97);
                 int pos=hash(bkp[i].head->data,capacity);
                 linkedlist temp = arr[pos];
                 temp=bkp[i];
             }
-    }*/
+    }
 
     int hash(std::string key,int n){
         int ret;
@@ -42,19 +47,22 @@ public:
     linearChaining(int initSize=10):size(0),capacity(initSize),arr(new linkedlist[initSize]){}
 
     void add(std::string key){
-
+        if(2*size>=capacity){
+            grow();
+        }
         int pos=hash(key,capacity);
         //uint64 spookyPos=SpookyHash::Hash64(&key, sizeof(key),97);
         //uint64_t pos=spookyPos&(initSize-1);
         linkedlist::iterator i(arr[pos]);
-
+        if(!i==0){
+            size++;
+        }
         while(!i){
             if(*i==key)
                 return;
             ++i;
         }
         arr[pos].addfront(key);
-        size++;
     }
 
     std::string find(std::string key){
--- main.cpp
+++ main.cpp
@@ -1,10 +1,8 @@
 #include "LinearChaning.h"
-#include <cstring>
 using namespace std;
 int main() {
-    linearChaining<int> myHash(1000000);
+    linearChaining<int> myHash(10000000);
     myHash.readList("C:\\Users\\Omkar Desai\\Documents\\C++\\LinearChaining\\dict.txt");
-    //cout<<myHash.find("bajirao")<<std::endl;
     myHash.checkList("C:\\Users\\Omkar Desai\\Documents\\C++\\LinearChaining\\check.txt");
     myHash.histogram();
     return 0;
