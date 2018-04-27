9a1c3a4ebb492d660ccb39a121d0f48615504e2c
swiftomkar <swiftomkar@gmail.com>
2018-04-27 14:37:59 -0400

    amazing hash!

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 #include "LinearChaning.h"
 using namespace std;
 int main() {
-    linearChaining myHash(50000000);
+    linearChaining myHash(500000);
     myHash.readList("C:\\Users\\Omkar Desai\\Documents\\C++\\LinearChaining\\dict.txt");
-    myHash.checkList("C:\\Users\\Omkar Desai\\Documents\\C++\\LinearChaining\\check.txt");
+    myHash.checkList("C:\\Users\\Omkar Desai\\Documents\\C++\\LinearChaining\\hw8.txt");
     myHash.histogram();
     return 0;
 }
