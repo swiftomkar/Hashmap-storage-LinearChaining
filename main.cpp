da39df54279a1efcda22332083ff43dc6e5029a6
swiftomkar <swiftomkar@gmail.com>
2018-05-02 13:43:15 -0400

    changed prime number

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include "LinearChaning.h"
 using namespace std;
 int main() {
-    linearChaining myHash(800000);
+    linearChaining myHash((1024*1024)*2);
     myHash.readList("C:\\Users\\Omkar Desai\\Documents\\C++\\LinearChaining\\dict.txt");
     myHash.checkList("C:\\Users\\Omkar Desai\\Documents\\C++\\LinearChaining\\hw8.txt");
     myHash.histogram();
